44d53802dbe2f51d86c2102df14668a61540ad79
Meelad <SoheilRavasani@Gmail.com>
2017-09-23 16:40:04 +0330

    add regex for statement syntax

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,13 +1,25 @@
 import re
 
-if_pattern = r'if\(.+\)(\n)*{(\n)?.+(\n)?}'
-while_pattern = r'while\(.+\)(\n)*{(\n)?.+(\n)?}'
-for_pattern = r'for\((.+=.+);(.+[\<\>]\=?.+);(.+);?\)(\n)*{(\n)?.+(\n)?}'
-lines = []
-while True:
-    line = input()
-    if line:
-        lines.append(line)
-    else:
-        break
-text = '\n'.join(lines)
+
+def check_syntax(text):
+    if_pattern = r'if\(.+\)(\n)*{(\n)?.+(\n)?}'
+    while_pattern = r'while\(.+\)(\n)*{(\n)?.+(\n)?}'
+    for_pattern = r'for\((.+=.+);(.+[\<\>]\=?.+);(.+);?\)(\n)*{(\n)?.+(\n)?}'
+
+    if 'if' in text:
+        if re.search(if_pattern, text):
+            print("ok")
+        else:
+            print('<< if >> syntax is wrong')
+
+    if 'while' in text:
+        if re.search(while_pattern, text):
+            print("ok")
+        else:
+            print('<< if >> syntax is wrong')
+
+    if 'for' in text:
+        if re.search(if_pattern, text):
+            print("ok")
+        else:
+            print('<< if >> syntax is wrong')
