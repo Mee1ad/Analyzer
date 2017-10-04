0aae85f27fa201733361866c824c50d35305ce61
Meelad <SoheilRavasani@Gmail.com>
2017-10-04 15:22:27 +0330

    bug fixed

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,25 +1,25 @@
 import re
 
 
-def check_syntax(text):
+def statement_syntax(text):
     if_pattern = r'if\(.+\)(\n)*{(\n)?.+(\n)?}'
     while_pattern = r'while\(.+\)(\n)*{(\n)?.+(\n)?}'
     for_pattern = r'for\((.+=.+);(.+[\<\>]\=?.+);(.+);?\)(\n)*{(\n)?.+(\n)?}'
 
     if 'if' in text:
         if re.search(if_pattern, text):
-            print("ok")
+            pass
         else:
             print('<< if >> syntax is wrong')
 
     if 'while' in text:
         if re.search(while_pattern, text):
-            print("ok")
+            pass
         else:
-            print('<< if >> syntax is wrong')
+            print('<< while >> syntax is wrong')
 
     if 'for' in text:
         if re.search(if_pattern, text):
-            print("ok")
+            pass
         else:
-            print('<< if >> syntax is wrong')
+            print('<< for >> syntax is wrong')
