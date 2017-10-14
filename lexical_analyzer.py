6fb8e9c5259fcfa88f725ef48ff5d10eacb87cd9
Meelad <SoheilRavasani@Gmail.com>
2017-10-14 14:48:59 +0330

    fix index error of lexical analyzer

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -9,32 +9,25 @@ def split():
         text2 = text
         global word
         word = re.findall(r'(\"?\w+\.?\w*\"?)', text)
-        # print('word=', word)
         op = re.findall(r'[\+\-\*\{\}\(\)\];]', text)
         rop = re.findall(r'[\=\<\>\!]=?', text)
         token = word + op + rop
         len_token = len(token)
         token_pos = []
         for i in range(len_token):
-            # if text2.index('if'):
-            # if text2.index(token[i]) in token_pos:
-            # text_to_be_replace = text2[:text2.index(token[i]) + 1]
-            # replaced_text = replace(text_to_be_replace, token[i])
-            # replaced_text += text2[text2.index(token[i]) + 1:]
-            # text2 = replaced_text
             token_pos.append(text2.index(token[i]))
             text2 = replace(text2, token[i])
-        # print('token_pos', token_pos, '\n')
-        # print('token: ', token, '\n')3
         token_dict = {}
         for i in range(len_token):
             token_dict[token_pos[i]] = token[i]
         # print(sorted(token_dict))
         token_key = sorted(token_dict)
-        sorted_token = {}
+        global word_dict
+        word_dict = {}
         for i in range(len(token_dict)):
-            sorted_token[token_key[i]] = token_dict[token_key[i]]
-        return sorted_token
+            word_dict[token_key[i]] = token_dict[token_key[i]]
+
+        return word_dict
 
 
 def replace(text, word):
@@ -79,23 +72,27 @@ def regex(dic):
     }
     global pattern
     l = list(dic.keys())
+    # print(l)
     for k in dic:
         for pattern in token_name:
             if re.match(token_name[pattern], dic[k]):
                 val = ''
                 if pattern == 'ident':
                     i = text.index(dic[k])
-                    print('i is:', i)
+                    #print('i is:', i)
                     if i not in l:
                         i = text.rindex(dic[k])
-                        print('rindex i:', i)
-                    print('dic[i]', dic[i])
+                        # print('rindex i:', i)
+                    #print('dic[i]', dic[i])
                     p = l.index(i)
-                    print(l , p  , ' - ' , dic[l[p]])
-                    if dic[l[p+1]] == '=':
-                        print('yeees' , dic[l[p]])
-                        j = word.index(dic[k])
-                        val = word[j + 1]
+                    # print(l , p  , ' - ' , dic[l[p]])
+                    try:
+                        if dic[l[p + 1]] == '=':
+                            # print('yeees' , dic[l[p]])
+                            j = word.index(dic[k])
+                            val = word[j + 1]
+                    except:
+                        pass
                 table.insert(dict(token=dic[k], type=pattern, value=val, position=k))
                 break
 
